cf620ec91013313f44d06f5bc847e5fe9e2deed5
dnyana <dnyaneshgawade903@gmail.com>
2021-08-23 20:44:48 +0530

    [Dnyanesh] Add : Add person in list

---
+++ PersonDataManagement/PersonData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonDataManagement
+{
+    public class PersonData
+    {
+        public static List<Person> list = new List<Person>();
+        public void AddPersonData(List<Person> list)
+        {
+            Person person = new Person();
+            Console.WriteLine("Enter SSN: ");
+            person.SSN = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter Name: ");
+            person.Name = Console.ReadLine();
+            Console.WriteLine("Enter Address: ");
+            person.Address = Console.ReadLine();
+            Console.WriteLine("Enter Age:");
+            person.Age = Convert.ToInt16(Console.ReadLine());
+            list.Add(person);
+            Console.WriteLine("Data Added");
+        }
+        public void Display(List<Person> list)
+        {
+            foreach (Person data in list)
+            {
+                Console.WriteLine("SSN is :" + data.SSN);
+                Console.WriteLine("Name :" + data.Name);
+                Console.WriteLine("Address :" + data.Address);
+                Console.WriteLine("Age :" + data.Age);
+            }
+        }
+        public void DataManagement()
+        {
+            int choice;
+            Console.WriteLine("\n1. for inserting record\n2. for display the record");
+            Console.WriteLine("Enter Your choice");
+            choice = Convert.ToInt16(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    AddPersonData(list);
+                    break;
+                case 2:
+                    Display(list);
+                    break;
+                default:
+                    Console.WriteLine("Oops you enter wrong input.. try again...");
+                    break;
+
+            }
+
+        }
+    }
+}
---
+++ PersonDataManagement/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PersonDataManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonData personData = new PersonData();
+            PersonData.list.Add(new Person(){ SSN = 1, Name="Dnyanesh", Address="Kolhapur",Age=24});
+            PersonData.list.Add(new Person(){ SSN = 2, Name = "Jay", Address = "Pune", Age = 15 });
+            PersonData.list.Add(new Person(){ SSN = 3, Name = "Rahul", Address = "Goa", Age = 45 });
+            PersonData.list.Add(new Person(){ SSN = 4, Name = "Mayuresh", Address = "Chennai", Age = 55});
+            PersonData.list.Add(new Person(){ SSN = 5, Name = "Raj", Address = "Delhi", Age = 70 });
+            personData.DataManagement();
+        }
+    }
+}
